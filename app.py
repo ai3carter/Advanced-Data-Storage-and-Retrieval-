260af47665a23629f11258649e0d77e736eac7f6
dr.cozy <51274767+ai3carter@users.noreply.github.com>
2019-10-11 16:02:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,22 @@ Base.prepare(engine, reflect=True)
 Station = Base.classes.station
 Measurement=Base.classes.measurement
 # Create our session (link) from Python to the DB
-
-
+session = Session(engine)
+# The function will return the max, min, and avg of temperature in the range of 
+# the start and end date that you type in
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
 # Flask Setup
 app = Flask(__name__)
@@ -48,8 +62,15 @@ def welcome():
 def precipitation():
     
     session = Session(engine)
-    results = session.query(Measurement.date,Measurement.prcp).all()
 
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+    begin_date = max_date - dt.timedelta(366)
+    results = session.query(func.strftime("%Y-%m-%d",Measurement.date),Measurement.prcp).\
+        filter(func.strftime("%Y-%m-%d",Measurement.date)>=begin_date).all()
+# strftime translates to 
+# "create formatted string for given time/date/datetime object according to specified format."
     all_prcp = []
     for date,prcp in results:
         prcp_dict={}
@@ -58,6 +79,7 @@ def precipitation():
         all_prcp.append(prcp_dict)
 # this list is not a dictionary
     return jsonify(all_prcp)
+
 
 
 @app.route("/api/v1.0/stations")
@@ -77,7 +99,13 @@ def station():
 def tob():
    
     session = Session(engine)
-    results = session.query(Measurement.station, Measurement.date,Measurement.tobs).all()
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+    begin_date = max_date - dt.timedelta(366)
+
+    results = session.query(Measurement.station, Measurement.date,Measurement.tobs).\
+        filter(func.strftime("%Y-%m-%d",Measurement.date)>=begin_date).all()
 
     # Convert list of tuples into normal list
     all_tobs = []
@@ -93,25 +121,22 @@ def tob():
 
 @app.route("/api/v1.0/<start>")
 def start(start):
-    
-    start_date=dt.date(start)
-
     session = Session(engine)
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).all()
-
+   
+    # find the last day 
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date= final_date_query[0][0]
+   
+    temps=calc_temps(start,max_date)
     
-    a=func.min(Measurement.tobs)
-    b=func.avg(Measurement.tobs)
-    c=func.max(Measurement.tobs)
+  
 
     all_start_tobs = []
-    for a,b,c in results:
-        start_dict = {}
-        start_dict["Max"] = a
-        start_dict["Avg"] = b
-        start_dict["Min"] = c
-        all_start_tobs.append(start_dict)
+    date_dict = {'start_date': start, 'end_date': max_date}
+    all_start_tobs.append(date_dict)
+    all_start_tobs.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
+    all_start_tobs.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
+    all_start_tobs.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
 
     return jsonify(all_start_tobs)
 
@@ -119,25 +144,19 @@ def start(start):
 @app.route("/api/v1.0/<start>/<end>")
 def startend(start,end):
 
-    start_date=dt.date(start)
-    end_date=dt.date(end)
-    session = Session(engine)
-    results=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
     
-    a=func.min(Measurement.tobs)
-    b=func.avg(Measurement.tobs)
-    c=func.max(Measurement.tobs)
+    # use the calc_temps func to get the temperatures
+    temps=calc_temps(start,end)
+    
 
-    all_start_end_tobs = []
-    for a,b,c in results:
-        start_end_dict = {}
-        start_end_dict["Max"] = a
-        start_end_dict["Avg"] = b
-        start_end_dict["Min"] = c
-        all_start_end_tobs.append(start_end_dict)
-
-    return jsonify(all_start_end_tobs)
+    #create a list
+    return_list = []
+    date_dict = {'start_date': start, 'end_date': end}
+    return_list.append(date_dict)
+    return_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
+    return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
+    return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
+    return jsonify(return_list)
 
 
 
